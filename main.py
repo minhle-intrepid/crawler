0157f382e31076361a4507b3b07abbea0cb4b1a3
Minh Le - Data Engineer <minh.le@intrepid.asia>
2022-03-15 15:59:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import itertools
 from module.module import *
 
@@ -8,12 +9,15 @@ if __name__ == '__main__':
                  "https://www.lazada.vn/shop/xiaomi-flagship-store",
                  "https://www.lazada.vn/shop/hogoto-shopp",
                  "https://www.lazada.vn/shop/sdvn-streetwear"]
-    for shop_url in shop_urls:
+    for index, shop_url in enumerate(shop_urls):
         lazada_crawler = LazadaCrawler(shop_url)
         data = lazada_crawler.get_all_product_infor_by_shop()
-        list(itertools.chain(meta_data, data))
-    shop_url = "https://shopee.vn/samsung_official_store"
-    shopee_crawler = ShopeeCrawler(shop_url)
-    data = shopee_crawler.get_all_product_infor_by_shop()
+        meta_data = list(itertools.chain(meta_data, data))
+        print("Done: ", index+1, "/", len(shop_urls))
+        if index+1 != len(shop_urls):
+            time.sleep(120)
+    # shop_url = "https://shopee.vn/samsung_official_store"
+    # shopee_crawler = ShopeeCrawler(shop_url)
+    # data = shopee_crawler.get_all_product_infor_by_shop()
     with open('./data/data.json', 'w', encoding='utf-8') as f:
         json.dump(meta_data, f, ensure_ascii=False, indent=4)
